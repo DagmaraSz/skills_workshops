13ff7361358848b13a7bdbaf7d6a29d028de2cd9
Dagmara <daga.sz@hotmail.com>
2016-09-28 11:13:23 +0100

    encapsulated

--- week-1/oop_1/lib/calculator.rb
+++ week-1/oop_1/lib/calculator.rb
@@ -1,14 +1,18 @@
-def add(number_1, number_2)
-  number_1 + number_2
+class CalculatorEngine
+  def add(number_1, number_2)
+    number_1 + number_2
+  end
+
+  def subtract(number_1, number_2)
+    number_1 - number_2
+  end
 end
 
-def subtract(number_1, number_2)
-  number_1 - number_2
+class Printer
+  def print_answer(answer)
+    "The Answer is: #{ answer }"
+  end
 end
 
-def print_answer(answer)
-  "The Answer is: #{ answer }"
-end
-
-puts print_answer(add(2, 3))
-puts print_answer(subtract(3, 2))
+#puts print_answer(add(2, 3))
+#puts print_answer(subtract(3, 2))
--- week-1/oop_1/spec/calculator_spec.rb
+++ week-1/oop_1/spec/calculator_spec.rb
@@ -1,35 +1,38 @@
 require 'calculator'
 
 describe 'A calculator' do
-  describe '#add' do
-    it 'adds two numbers' do
-      expect(add(2, 3)).to eq 5
-      expect(add(6, 4)).to eq 10
-      expect(add(0, 0)).to eq 0
-      expect(add(-1, -1)).to eq -2
-      expect(add(Float::INFINITY, 0)).to eq Float::INFINITY
-      expect(add(Float::INFINITY, Float::INFINITY)).to eq Float::INFINITY
-    end
-  end
+ describe '#add' do
+   it 'adds two numbers' do
+     engine = CalculatorEngine.new
+     expect(engine.add(2, 3)).to eq 5
+     expect(engine.add(6, 4)).to eq 10
+     expect(engine.add(0, 0)).to eq 0
+     expect(engine.add(-1, -1)).to eq -2
+     expect(engine.add(Float::INFINITY, 0)).to eq Float::INFINITY
+     expect(engine.add(Float::INFINITY, Float::INFINITY)).to eq Float::INFINITY
+   end
+ end
 
-  describe '#subtract' do
-    it 'subtracts two numbers' do
-      expect(subtract(3, 2)).to eq 1
-      expect(subtract(6, 4)).to eq 2
-      expect(subtract(0, 0)).to eq 0
-      expect(subtract(-1, -1)).to eq 0
-      expect(subtract(Float::INFINITY, 0)).to eq Float::INFINITY
-      expect(subtract(Float::INFINITY, Float::INFINITY)).to be_nan
-    end
-  end
+ describe '#subtract' do
+   it 'subtracts two numbers' do
+     engine = CalculatorEngine.new
+     expect(engine.subtract(3, 2)).to eq 1
+     expect(engine.subtract(6, 4)).to eq 2
+     expect(engine.subtract(0, 0)).to eq 0
+     expect(engine.subtract(-1, -1)).to eq 0
+     expect(engine.subtract(Float::INFINITY, 0)).to eq Float::INFINITY
+     expect(engine.subtract(Float::INFINITY, Float::INFINITY)).to be_nan
+   end
+ end
 
-  describe '#print_answer' do
-    it 'pretty prints the answer' do
-      expect(printer.print_answer(4)).to eq "The Answer is: 4"
-      expect(printer.print_answer(6)).to eq "The Answer is: 6"
-      expect(printer.print_answer(-1)).to eq "The Answer is: -1"
-      expect(printer.print_answer(0)).to eq "The Answer is: 0"
-      expect(printer.print_answer(Float::INFINITY)).to eq "The Answer is: Infinity"
-    end
-  end
+ describe '#print_answer' do
+   it 'pretty prints the answer' do
+     printer = Printer.new
+     expect(printer.print_answer(4)).to eq "The Answer is: 4"
+     expect(printer.print_answer(6)).to eq "The Answer is: 6"
+     expect(printer.print_answer(-1)).to eq "The Answer is: -1"
+     expect(printer.print_answer(0)).to eq "The Answer is: 0"
+     expect(printer.print_answer(Float::INFINITY)).to eq "The Answer is: Infinity"
+   end
+ end
 end
